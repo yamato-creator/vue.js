a76b1348a641e15996a19843ab0517a493402294
yamato-creator <yamato-creator>
2021-03-07 12:06:07 +0900

    todoリスト

--- 7-1/script/script.js
+++ 7-1/script/script.js
@@ -7,10 +7,12 @@ new Vue({
     };
   },
   methods: {
+    //テキストボックスからの入力を引数して受け取りtextプロパティにセット
     inputText(e) {
       this.text = e.target.value;
     },
     addTodo() {
+      //textプロパティが空なら何も起きない
       if (!this.text) return;
 
       const text = this.text;
