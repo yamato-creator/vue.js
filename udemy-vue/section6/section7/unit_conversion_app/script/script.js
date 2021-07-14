f96633973d27e49c7bf0dea6ee459d3458d9a1b7
yamato-creator <dydydydydydy5656@gmail.com>
2021-07-14 16:44:31 +0900

    add

---
+++ udemy-vue/section6/section7/unit_conversion_app/script/script.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    km: 0,
+    m: 0,
+    mm:0
+  },
+  watch: {
+    km: function(value) {
+      this.km = value
+      this.m = value * 1000
+      this.mm = value * 1000000
+    },
+    m: function(value) {
+      this.km = value / 1000
+      this.m = value
+      this.mm = value * 1000
+    },
+    mm: function(value) {
+      this.km = value / 1000000
+      this.m = value / 1000
+      this.mm = value
+    },
+  }
+})
