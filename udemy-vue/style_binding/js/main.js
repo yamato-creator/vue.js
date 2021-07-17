fb3770f1264c8bfc55c45828c5a954e89251934e
yamato-creator <dydydydydydy5656@gmail.com>
2021-07-17 13:59:10 +0900

    add

---
+++ udemy-vue/style_binding/js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		// isLarge: true,
+		// hasError: true
+
+		// largeClass: 'large',
+		// dangerClass: 'text-danger'
+
+		classObject: {
+			large: true,
+			'text-danger': true
+		}
+	}
+})
