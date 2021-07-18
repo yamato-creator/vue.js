7d2ae0f713129f533b4a0ed1b91c8212696bdb54
yamato-creator <dydydydydydy5656@gmail.com>
2021-07-18 14:52:29 +0900

    add

---
+++ udemy-vue/form_binding/js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		// message: 'hello!vue.js',
+		checked: false,
+		colors: [],
+		color: '',
+		// selected: ''
+		selected: [],
+		message: '',
+		age: 0
+	}
+})
